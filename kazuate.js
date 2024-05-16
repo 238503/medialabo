16822a598f4804ecbabfcadecdd41496dc45971d
G385032023 <238503@st.takushoku-u.ac.jp>
2024-05-16 17:34:33 +0900

    ab

--- kazuate.js
+++ kazuate.js
@@ -17,7 +17,19 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+  kaisu = kaisu + 1;
+  console.log(kaisu + "回目の予想:" + yoso);
+  if(kaisu > 3){
+    console.log("答えは" + kotae + "でした．すでにゲームは終わっています");
+  }else{
+    if(kotae === yoso){
+      console.log("正解です．おめでとう!");
+    }else if(kotae < yoso){
+      console.log("");
+    }else if(kotae < yoso){
+      
+    }
+  }
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
