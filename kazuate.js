75fbdb02751e8249a91a4c9842e1cd7de3c95373
G385032023 <238503@st.takushoku-u.ac.jp>
2024-05-16 21:21:46 +0900

    kazuate

--- kazuate.js
+++ kazuate.js
@@ -19,15 +19,19 @@ function hantei() {
   let yoso = 4;
   kaisu = kaisu + 1;
   console.log(kaisu + "回目の予想:" + yoso);
-  if(kaisu > 3){
-    console.log("答えは" + kotae + "でした．すでにゲームは終わっています");
-  }else{
-    if(kotae === yoso){
-      console.log("正解です．おめでとう!");
-    }else if(kotae < yoso){
-      console.log("");
-    }else if(kotae < yoso){
-      
+  if (kaisu > 3) {
+    console.log("答えは " + kotae + " でした。すでにゲームは終わっています。");
+  } else if (yoso === kotae) {
+    console.log("正解です。おめでとう!");
+  } else {
+    if (kaisu === 3) {
+      console.log("まちがい。残念でした答えは " + kotae + " です。");
+    } else if (kaisu <= 2) {
+      if (yoso < kotae) {
+        console.log("まちがい。答えはもっと大きいですよ。");
+      } else {
+        console.log("まちがい。答えはもっと小さいですよ。");
+      }
     }
   }
   // 課題3-1: 正解判定する
